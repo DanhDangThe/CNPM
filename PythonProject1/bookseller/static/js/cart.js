9d7a4418fa3af6ee71cd075b59b42997e038da68
DangTheDanh <danhvisk2004@gmail.com>
2024-12-04 06:18:19 +0700

    Initial commit

---
+++ PythonProject1/bookseller/static/js/cart.js
@@ -0,0 +1,94 @@
+function toggleDropdown(show) {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.style.display = show ? 'block' : 'none';
+}
+
+const accountIcon = document.getElementById('accountIcon');
+
+accountIcon.addEventListener('mouseenter', () => toggleDropdown(true));
+accountIcon.addEventListener('mouseleave', () => toggleDropdown(false));
+
+function addToCart( id, name, price) {
+    event.preventDefault();
+    fetch('/api/add-cart', {
+        method: 'post',
+        body: JSON.stringify({
+            'id': id,
+            'name': name,
+            'price': price
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(res) {
+        console.info(res);
+        return res.json();
+    })
+    .then(function(data) {
+        console.info(data);
+        let counter=document.getElementsByClassName('cartCounter')
+        for( let i=0;i<counter.length;i++)
+            counter[i].innerText=data.total_quantity
+    })
+    .catch(function(err) {
+        console.error(err);
+    });
+}
+function pay(){
+    if(confirm('Ban Chac Chan Muon Thanh Toan ?')==true){
+        fetch('/api/pay', {
+            method: 'post'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.code == 200)
+                location.reload();
+        })
+        .catch(err => { console.error(err);
+        }
+        );
+    }
+}
+function updateCart(id,obj){
+    fetch('/api/update-cart',{
+
+        method:'put',
+        body:JSON.stringify( {
+        'id':id,
+        'quantity':parseInt(obj.value)
+
+        }),
+        headers:{
+        'Content-Type': 'application/json'
+        }
+        }).then(res => res.json()).then(data =>{
+            let counter=document.getElementsByClassName('cartCounter');
+            for( let i=0;i<counter.length;i++)
+                counter[i].innerText=data.total_quantity;
+            let amount=document.getElementById('total-amount')
+            amount.innerText =new Intl.NumberFormat().format(data.total_amount)
+        })
+}
+function deleteCart(id){
+    if(confirm("ban co cac chan xoa k")==true)
+    {
+         fetch('/api/delete-cart/'+id,{
+
+        method:'delete',
+
+        headers:{
+        'Content-Type': 'application/json'
+        }
+        }).then(res => res.json()).then(data =>{
+            let counter=document.getElementsByClassName('cartCounter');
+            for( let i=0;i<counter.length;i++)
+                counter[i].innerText=data.total_quantity;
+            let amount=document.getElementById('total-amount')
+            amount.innerText =new Intl.NumberFormat().format(data.total_amount)
+             let e =document.getElementById("product"+id)
+             e.style.display= "none"
+        }).catch(err => console.error(err))
+    }
+
+}
---
+++ PythonProject1/bookseller/utils.py
@@ -0,0 +1,46 @@
+import json, os
+from bookseller import app,db
+from flask import render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import  current_user
+from bookseller.model import Category,Product, User
+
+import hashlib
+def read_json(path):
+    with open(path,"r") as f:
+        return json.load(f)
+def load_categories():
+    return Category.query.order_by("id").all()
+def load_products(cate_id=None,kw=None,from_price=None,to_price=None,page=1):
+    products = Product.query
+    if kw:
+        products = products.filter(Product.name.contains(kw))
+    if cate_id:
+        products = products.filter(Product.category_id == cate_id)
+    page_size = app.config['PAGE_SIZE']
+    start =(page-1)*page_size
+    end=start+page_size
+    return products.slice(start,end).all()
+def cout_products():
+    return Product.query.count()
+def check_login(username,password):
+    if username and password :
+        password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+        return User.query.filter(User.username.__eq__(username.strip()),User.password.__eq__(password)).first()
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
+def add_user(name,username,password,**kwargs):
+    password=str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    user= User(name=name.strip() ,username=username.strip(),password=password,avatar=kwargs.get('avatar'))
+    db.session.add(user)
+    db.session.commit()
+def count_cart(cart):
+    total_quantity,total_amount=0,0
+    if cart:
+        for c in cart.values():
+            total_quantity+=c['quantity']
+            total_amount+=c['quantity']*c['price']
+    return {
+        "total_amount":total_amount,
+        "total_quantity":total_quantity
+    }
